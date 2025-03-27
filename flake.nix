ea737868f835507f52a51cbf21fbd6c98d6fe742
permalik <permalik@protonmail.com>
2025-03-26 23:44:27 -0500

    feat: impl flake-utils, update prompt

--- flake.nix
+++ flake.nix
@@ -1,43 +1,35 @@
 {
-  description = "permalik go_curricula";
+  description = "go_curricula";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/release-24.11";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = {
     self,
     nixpkgs,
-    ...
-  }: let
-    systems = ["x86_64-linux" "aarch64-darwin"];
-    forAllSystems = f:
-      builtins.listToAttrs (map (system: {
-          name = system;
-          value = f system;
-        })
-        systems);
-  in {
-    devShells = forAllSystems (system: let
-      pkgs = import nixpkgs {
-        inherit system;
-        config.allowUnfree = false;
-      };
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = false;
+        };
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.alejandra
+            pkgs.go_1_23
+            pkgs.golangci-lint
+          ];
 
-      shell = pkgs.mkShell {
-        buildInputs = [
-          pkgs.alejandra
-          pkgs.go_1_23
-          pkgs.golangci-lint
-        ];
-
-        shellHook = ''
-          # Custom Prompt
-          export PS1="\[\e[1;32m\][devshell] \w \$\[\e[0m\]\n❯ "
-        '';
-      };
-    in {
-      default = shell;
-    });
-  };
+          shellHook = ''
+            # Custom Prompt
+            export PS1="\n\[\e[1;32m\][devshell](go) \w\n❯ \[\e[0m\]"
+          '';
+        };
+      }
+    );
 }
