f1a20286edb3eeb31d543e1ad288d2013d3e8cc3
permalik <permalik@protonmail.com>
2025-04-03 21:04:52 -0500

    refactor: complete precommit

--- .precommit.sh
+++ .precommit.sh
@@ -8,41 +8,37 @@ pre-commit clean
 cat << 'EOF'
 #!/bin/bash
 
-# Format and lint
-echo "Go format and lint.."
-
-# # Find all Go files in the repository (staged or unmodified)
-# ALL_GO_FILES=$(git ls-files -- '*.go')
 #
-# if [ -z "$ALL_GO_FILES" ]; then
-#     echo "No Go files to format or lint."
-#     exit 0
-# fi
+# Go Format and Lint
+#
+
+echo "Start: Go Format and Lint"
 
 echo "Execute: goimports"
 goimports -l -w .
 
-# # Add formatted files back to the staging area
-# git add $ALL_GO_FILES
-
-# Run golangci-lint on all Go files
 echo "Execute: golangci-lint"
 golangci-lint run --fix --timeout=5m
 
-# Fail commit on linting errors
 if [ $? -ne 0 ]; then
     echo "Error: golangci-lint failed."
     exit 1
 fi
 
-echo "Complete: Go format and lint"
+echo "Complete: Go Format and Lint"
+
+#
+# Pre-Commit
+#
+
 EOF
 
-    cat .git/hooks/pre-commit
+cat .git/hooks/pre-commit
+
 } > .git/hooks/pre-commit.tmp
 
 mv .git/hooks/pre-commit.tmp .git/hooks/pre-commit
 
 sudo chmod +x .git/hooks/pre-commit
 
-echo "Appended Go formatting and linting"
+echo "Success: Appended Go format and lint"
