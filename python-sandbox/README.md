53fd2533fc114355f9e300acf74703bc8f4ae617
permalik <permalik@protonmail.com>
2025-09-13 14:54:54 -0500

    wip: impl python

---
+++ python-sandbox/README.md
@@ -0,0 +1,2 @@
+# python-sandbox
+sandbox:Declarative Python Environment
---
+++ python-sandbox/main.py
@@ -0,0 +1,12 @@
+# main.py
+"""
+python_sandbox
+"""
+
+
+def main():
+    print("started python-sandbox")
+
+
+if __name__ == "__main__":
+    main()
